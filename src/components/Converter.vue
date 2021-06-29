83fa7697d06181565fd7f8ffc451230671193cdc
Gabrinne <gabrinneloyse@hotmail.com>
2021-06-28 23:45:10 -0300

    redefinindo layout

--- src/components/Converter.vue
+++ src/components/Converter.vue
@@ -1,6 +1,175 @@
 <template>
-  <v-container grid-list-md>
-    <v-window v-model="step">
+  <v-container>
+    <v-row style="border: 5px solid yellow" class="mb-6">
+      <v-col cols="12" sm="12">
+        <v-card class=" text-center" flat>
+          LE/TIP
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="12" xs="12" md="12" sm="12" class="pa-0 ">
+        <v-window v-model="step" class="pa-3">
+          <v-window-item :value="1">
+            <v-row style="border: 2px solid black" class="">
+              <v-col
+                cols="12 "
+                style="border: 3px solid green"
+                xs="12"
+                md="6"
+                class=" d-flex justify-center align-center"
+                sm="6"
+              >
+                <label>EUR</label>
+                <v-switch
+                  color="deep-purple accent-2 "
+                  class="pl-4 font-weight-light"
+                  v-model="base_currency"
+                  true-value="USD"
+                  false-value="EUR"
+                  inset
+                ></v-switch>
+                <label>USD</label>
+              </v-col>
+
+              <v-col
+                cols="12"
+                xs="12"
+                md="6"
+                class=" d-flex justify-center "
+                sm="6"
+                style="border: 3px solid red"
+              >
+                <vuetify-money
+                  color="deep-purple accent-2"
+                  class=" mt-5"
+                  placeholder="Digite o valor da conta"
+                  v-model="amount"
+                  outlined
+                ></vuetify-money>
+              </v-col>
+              <v-col
+                cols="12"
+                xs="12"
+                md="6"
+                sm="6"
+                style="border: 3px solid red"
+              >
+                <v-card class="pa-2 justify-space-around text-center">
+                  CONTA
+                </v-card>
+                <v-card class="pa-2   text-center">
+                  CONTA 1
+                </v-card>
+                <v-card class="pa-2   text-center">
+                  CONTA 1
+                </v-card>
+              </v-col>
+              <v-col
+                cols="12"
+                xs="12"
+                md="6"
+                sm="6"
+                style="border: 3px solid red"
+              >
+                <v-card class="pa-2   text-center">
+                  CONTA
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-window-item>
+
+          <v-window-item :value="2">
+            <v-row style="border: 2px solid black" class="d-flex">
+              <v-col cols="12" xs="12" md="6" sm="6">
+                <v-card class="pa-2  text-center ">
+                  CONTA
+                </v-card>
+              </v-col>
+              <v-col cols="12" xs="12" md="6" sm="6">
+                <v-card class="pa-2   text-center">
+                  CONTA
+                </v-card>
+              </v-col>
+              <v-col cols="12" xs="12" md="6" sm="6">
+                <v-card class="pa-2 justify-space-around text-center">
+                  CONTA
+                </v-card>
+              </v-col>
+              <v-col cols="12" xs="12" md="6" sm="6">
+                <v-card class="pa-2   text-center">
+                  CONTA
+                </v-card>
+              </v-col>
+              <v-col cols="12" xs="12" md="6" sm="6">
+                <v-card class="pa-2   text-center">
+                  CONTA
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-window-item>
+        </v-window>
+      </v-col>
+    </v-row>
+    <!-- 
+      <v-col cols="12" xs="12" md="12" sm="12" style="border: 5px solid red">
+        <v-card class="pa-2 text-center">
+          CONTA
+        </v-card>
+      </v-col>
+      <v-col cols="12" xs="12" md="12" sm="12" style="border: 5px solid red">
+        <v-card class="pa-2 text-center">
+          VALOR
+        </v-card>
+      </v-col>
+      <v-col cols="12" xs="12" md="12" sm="12" style="border: 5px solid red">
+        <v-card class="pa-2 text-center">
+          GORJETA 13%
+        </v-card>
+      </v-col>
+      <v-col cols="12" xs="12" md="12" sm="12" style="border: 5px solid red">
+        <v-card class="pa-2 text-center">
+          TOTAL
+        </v-card>
+      </v-col>
+    </v-row> -->
+
+    <!--    <v-row>
+        <v-col cols="12" xs="12" md="12" sm="6" style="border: 5px solid red">
+    <v-window-item :value="2">
+          <v-card class="pa-2 text-center">
+            PESSOAS
+          </v-card>
+        </v-col>
+        <v-col cols="12" xs="12" md="12" sm="6" style="border: 5px solid red">
+          <v-card class="pa-2 text-center">
+            POR PESSOA
+          </v-card>
+        </v-col>
+        <v-col cols="12" xs="12" md="12" sm="6" style="border: 5px solid red">
+          <v-card class="pa-2 text-center">
+            EM R$
+          </v-card>
+        </v-col>
+        <v-col cols="12" xs="12" md="12" sm="6" style="border: 5px solid red">
+          <v-card class="pa-2 text-center">
+            EM R$
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-window-item>  -->
+
+    <v-speed-dial bottom right direction="top" fixed>
+      <template v-slot:activator>
+        <v-btn color="light-green accent-4" dark fab @click="step++">
+          >
+        </v-btn>
+      </template>
+    </v-speed-dial>
+  </v-container>
+
+  <!-- <v-window v-model="step">
       <v-speed-dial
         bottom
         right
@@ -20,6 +189,7 @@
         <v-layout row wrap class="mb-5">
           <v-flex
             xs12
+            style="border: 1px solid red"
             md12
             sm12
             class="text-center text-xl-h2 text-h4 ma-2 font-weight-light"
@@ -29,7 +199,13 @@
         </v-layout>
 
         <v-layout row wrap class="mb-5">
-          <v-flex class="pa-2 d-flex justify-center" xs12 md6 sm6>
+          <v-flex
+            class="pa-2 d-flex justify-center"
+            xs12
+            md6
+            sm6
+            style="border: 1px solid red"
+          >
             <v-card flat class="pa-2">
               <v-layout d-flex align-center class="pa-2">
                 <span class="font-weight-light">EUR</span>
@@ -84,8 +260,7 @@
           @number-people="getPeoples"
         />
       </v-window-item>
-    </v-window>
-  </v-container>
+    </v-window> -->
 </template>
 
 <script>
